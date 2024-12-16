c98109e8b749d8c05e8e2f621c05f3927d60e62c
FilhoDeEmer <141789981+FilhoDeEmer@users.noreply.github.com>
2024-12-16 17:55:28 -0300

    Realização do primeiro teste

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join( __dirname, 'public')));
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota principal que envia o arquivo index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use(port, () => {
-    console.log('Servidor rodando em http://localhost:${port}');
+// Iniciar o servidor na porta especificada
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
